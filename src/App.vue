ab885cfe35e5247f214928c014c85d79d4265a29
Lcdevi <caillauxlaura@gmail.com>
2021-01-26 20:08:59 +0100

    clean boilerplate vue create app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/">Home</router-link> | -->
+    <router-link to="/">Nos produits</router-link>
+    <router-link to="/">Le concept</router-link>
+    <router-link to="/">Avis</router-link>
+    <router-link to="/">Pub tv</router-link>
+    <router-link to="/">Mon compte</router-link>
+    <router-link to="/">Mag</router-link>
+    <router-link to="/">Contactez-nous</router-link>
   </div>
   <router-view/>
 </template>
@@ -11,20 +17,12 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
+  /* text-align: center; */
   color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: rgb(73, 73, 73);
+  text-decoration: none;
 }
 </style>
