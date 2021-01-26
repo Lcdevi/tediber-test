eccff3ba245919157da2781be39519e58b9fcf74
Lcdevi <caillauxlaura@gmail.com>
2021-01-26 20:45:56 +0100

    fetch command datas and client in home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,28 @@
 <template>
   <h1>TEDIBER TEST</h1>
+
+  <div>
+    <h3>informations commande:</h3>
+    <p> id de la commande: {{ commandes.id_cmd }} </p>
+    <p> numéro de commande: {{ commandes.numero_cmd }} </p>
+    <p> date de la commande: {{ commandes.date_cmd }} </p>
+    <p> date d'expedition: {{ commandes.date_exp }} </p>
+    <p> date de récéption: {{ commandes.date_reception }} </p>
+    <p> adresse de livraison: {{ commandes.adresse_livraison }} </p>
+    <p v-if="this.paiement === 'visa'"> paiement visa </p>
+    <p v-if="this.paiement === 'paypal'"> paiment paypal </p>
+    <p v-if="this.paiement === 'mastercard'"> paiement mastercard </p>
+    <h3>articles:</h3>
+    <div v-for="article in articles" :key="article.id_article">
+      <p> {{ article.designation }} </p>
+      <p> {{ article.prix }} €- </p>
+    </div>
+    <h3>informations client:</h3>
+    <p> id du client: {{ client.client_id }} </p>
+    <p> prénom du client: {{ client.client_prenom }} </p>
+    <p> nom du client: {{ client.client_nom }} </p>
+    <p> client téléphone: {{ client.client_tel }} </p>
+  </div>
 </template>
 
 <script>
@@ -7,9 +30,56 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
+
   name: 'Home',
+  
+  data() {
+    return {
+      commandes: [],
+      articles: [],
+      paiement: "",
+      client: {}
+    }
+  },
+  
   components: {
     HelloWorld
+  },
+
+  mounted() {
+    fetch('http://localhost:3000/commandes')
+      .then(res => res.json())
+      .then(data => {
+        console.log("data[0] >>>>>>>>>>>> ", data[0]);
+        this.commandes = data [0];
+        this.articles = data[0].articles;
+        if(data[0].paiement === "visa") {
+          this.paiement = "visa"
+        } else if(data[0].paiement === "paypal") {
+          this.paiement = "paypal"
+        } else {
+          this.paiement = "mastercard"
+        }
+      })
+      .catch(err => console.log(err.message))
+
+      fetch('http://localhost:3000/clients')
+        .then(res => res.json())
+        .then(data => {
+          console.log("data clients >>>>>>>>>>", data)
+          console.log("client id >>>>>>>", this.commandes.client_id)
+          let indexClient = data.findIndex(i => i.client_id === this.commandes.client_id)
+          this.client = data[indexClient]
+          console.log("le client >>>>>>>>", data[indexClient])
+        })
   }
+
+
+
+
+
+
+
+
 }
 </script>
